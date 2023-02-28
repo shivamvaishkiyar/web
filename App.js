a554f291a1e3f664581c06b9e2c8765cc592e57a
shivamvaishkiyar <104146996+shivamvaishkiyar@users.noreply.github.com>
2023-02-28 13:16:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import { StatusBar } from 'expo-status-bar';
+import { TouchableOpacity } from 'react-native';
+import { StyleSheet,
+   Text,
+    View , 
+    TouchableOpacity
+     } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+     
+    </View>
+  );
+}
+
